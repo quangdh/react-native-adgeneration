f2c2967914857312f482afe4ed891e5718800043
chuross <azarasi.mo.mo@gmail.com>
2018-05-24 20:40:19 +0900

    reformat code

--- android/src/main/java/com/github/chuross/rn/RNAdGenerationModule.java
+++ android/src/main/java/com/github/chuross/rn/RNAdGenerationModule.java
@@ -1,22 +1,19 @@
-
 package com.github.chuross.rn;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNAdGenerationModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNAdGenerationModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNAdGenerationModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNAdGeneration";
-  }
+    @Override
+    public String getName() {
+        return "RNAdGeneration";
+    }
 }
