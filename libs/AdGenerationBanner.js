0da881bf8e1426f5fe08c0262222e3e62c99021e
chuross <azarasi.mo.mo@gmail.com>
2018-05-25 00:38:20 +0900

    add console log to measure

--- libs/AdGenerationBanner.js
+++ libs/AdGenerationBanner.js
@@ -30,6 +30,7 @@ export default class AdGenerationBanner extends Component {
 
   _handleOnMeasure(event) {
     const { width, height } = event.nativeEvent;
+    console.log(event.nativeEvent);
     this.setState({
       style: { width, height }
     });
